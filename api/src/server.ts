931f4aeccc962a056720252c13f6477dda2a163e
Manuel Jiménez <man.jime@gmail.com>
2019-11-28 14:17:45 -0500

    static site init

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,26 +1,31 @@
 import "dotenv/config";
 import "reflect-metadata";
+import express from "express";
 import { createConnection } from "typeorm";
-import { ApolloServer } from "apollo-server";
+import { ApolloServer } from "apollo-server-express";
 import { buildSchema } from "type-graphql";
-import { Context } from "apollo-server-core";
 import jwt from "jsonwebtoken";
-// import mongoose from "mongoose";
 
 async function bootstrap(): Promise<void> {
-  createConnection();
-
   try {
+    createConnection();
+
+
+
     const schema = await buildSchema({
       resolvers: [__dirname + "/resolvers/**/*.ts"],
       authChecker: ({ context }, roles) => {
-        return roles.includes(context.user.role);
+        if (context.user) {
+          return roles.includes(context.user.role);
+        } else {
+          return false;
+        }
       }
     });
 
     const server = new ApolloServer({
       schema,
-      context: ({ req }): Context => {
+      context: ({ req }) => {
         const token = (req.headers.authorization || "").split(" ")[1];
         try {
           return { user: jwt.verify(token, process.env.JWT_SECRET || "s3cret") };
@@ -28,11 +33,16 @@ async function bootstrap(): Promise<void> {
           return { user: undefined };
         }
       },
-      cors: true,
       debug: false
     });
 
-    server.listen({ port: 4000 }, () => {
+
+    const app = express();
+    app.use(express.static(__dirname + '/public'));
+
+    server.applyMiddleware({ app });
+
+    app.listen({ port: 4000 }, () => {
       console.log(`Server is running on http://localhost:4000${server.graphqlPath}`);
     });
   } catch (error) {
