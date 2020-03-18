abb703b84fa0a44649807e1f3e04355b4b1b6c78
Manuel Jiménez <man.jime@gmail.com>
2020-03-18 18:09:03 -0500

    cambips para hacer el packing

---
+++ backend/scripts/postpack.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+PACKAGE_NAME=($(echo $npm_package_name | cut -c2- | awk -F'/' '{ print $1, $2 }'))
+SCOPE=${PACKAGE_NAME[0]}
+PACKAGE_NAME=${PACKAGE_NAME[1]}
+VERSION=$(echo $npm_package_version)
+
+
+cd ../../dist
+rm -rf $SCOPE-$PACKAGE_NAME.zip package
+tar -xzf ../src/delete-note/$SCOPE-$PACKAGE_NAME-$VERSION.tgz
+rm ../src/delete-note/$SCOPE-$PACKAGE_NAME-$VERSION.tgz
+cd package
+zip -qr ../$SCOPE-$PACKAGE_NAME.zip .
