9259fcb9d757189a28ad1ebef6dc647f5ac9f634
Manuel Jiménez <man.jime@gmail.com>
2020-03-20 08:32:53 -0500

    cambios iniciales

--- backend/src/delete-note/index.ts
+++ backend/src/delete-note/index.ts
@@ -1,6 +1,19 @@
 import { APIGatewayProxyWithCognitoAuthorizerHandler, APIGatewayProxyWithCognitoAuthorizerEvent, Context, APIGatewayProxyResult } from 'aws-lambda';
 
-export const hello: APIGatewayProxyWithCognitoAuthorizerHandler = async (_event: APIGatewayProxyWithCognitoAuthorizerEvent, _context: Context): Promise<APIGatewayProxyResult> => {
+import * as AWS from 'aws-sdk';
+import * as uuid from 'uuid';
+
+const db = new AWS.DynamoDB.DocumentClient();
+
+export const handler: APIGatewayProxyWithCognitoAuthorizerHandler = async (_event: APIGatewayProxyWithCognitoAuthorizerEvent, _context: Context): Promise<APIGatewayProxyResult> => {
+    await db.put({
+        TableName: process.env.TABLE_NAME!,
+        Item: {
+            userId: uuid.v4(),
+            noteId: uuid.v4()
+        }
+    }).promise();
+
     return Promise.resolve<APIGatewayProxyResult>({
         statusCode: 200,
         body: JSON.stringify({
