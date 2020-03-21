de4775ed75e4d87c60a11bf70f6c65d92993003b
Manuel Jiménez <man.jime@gmail.com>
2020-03-20 20:29:22 -0500

    cambios finales

--- backendv2/scripts/check-updates.sh
+++ backendv2/scripts/check-updates.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 echo "> updating root..."
-npx npm-check-updates --upgrade --packageFile ./package.json
+npm-check-updates --upgrade --packageFile ./package.json
+SCOPE=$(json -f ./package.json name)
 
-for dir in functions/*; do
-    if [ -d ${dir} ]; then
-        echo "> updating function $(basename $dir)..."
-        npx npm-check-updates --upgrade --packageFile ${dir}/package.json
+for DIR in functions/*; do
+    if [ -d ${DIR} ]; then
+        echo "> updating function $(basename $DIR)..."
+        json --in-place -f ${DIR}/package.json -e "this.name = '@$SCOPE/$(basename $DIR)'"
+        npm-check-updates --upgrade --packageFile ${DIR}/package.json
         echo ""
     fi
 done
