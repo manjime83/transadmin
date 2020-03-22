f417c96e7870cc7395a9e4b4f4a01a36332f165a
Manuel Jiménez <man.jime@gmail.com>
2020-03-22 18:42:15 -0500

    actualizaci'on del api

--- backendv2/functions/get-notes/index.ts
+++ backendv2/functions/get-notes/index.ts
@@ -4,12 +4,12 @@ export const handler: lambda.APIGatewayProxyWithCognitoAuthorizerHandler = async
   event: lambda.APIGatewayProxyWithCognitoAuthorizerEvent,
   context: lambda.Context
 ): Promise<lambda.APIGatewayProxyResult> => {
-  console.log(JSON.stringify(event), JSON.stringify(context));
-
   return Promise.resolve<lambda.APIGatewayProxyResult>({
     statusCode: 200,
     body: JSON.stringify({
       message: Math.floor(Math.random() * 10),
+      event,
+      context,
     }),
   });
 };
