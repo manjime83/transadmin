bbe920dc8d91cb86c2acd60a12d320fc51ba60bc
Manuel Jiménez <man.jime@gmail.com>
2020-03-24 06:53:09 -0500

    starting notes

--- backend/functions/notes/index.ts
+++ backend/functions/notes/index.ts
@@ -1,18 +1,53 @@
 import { APIGatewayProxyHandler, APIGatewayProxyEvent, Context, APIGatewayProxyResult } from "aws-lambda";
-import * as uuid from "uuid";
+// import * as uuid from "uuid";
+// import * as AWS from "aws-sdk";
+
+// const dynamodb = new AWS.DynamoDB.DocumentClient();
 
 export const handler: APIGatewayProxyHandler = async (
   event: APIGatewayProxyEvent,
   context: Context
 ): Promise<APIGatewayProxyResult> => {
-  switch (context.clientContext) {
-    default:
+  switch (event.requestContext.routeKey) {
+    case "POST /notes":
+      if (event.body === null) {
+        return {
+          statusCode: 500,
+          body: JSON.stringify({
+            requestId: context.awsRequestId,
+            messahe: "bad request",
+            event,
+            context,
+          }),
+        };
+      }
+
+      // const { description } = JSON.parse(event.body);
+
       return {
         statusCode: 200,
         body: JSON.stringify({
-          uuid: uuid.v4(),
+          requestId: context.awsRequestId,
+          messahe: "estoy en listar",
           event,
           context,
+        }),
+      };
+
+    case "GET /notes/{id}":
+      return {
+        statusCode: 200,
+        body: JSON.stringify({
+          requestId: context.awsRequestId,
+          messahe: "estoy en listar",
+        }),
+      };
+
+    default:
+      return {
+        statusCode: 404,
+        body: JSON.stringify({
+          message: "Not Found",
         }),
       };
   }
