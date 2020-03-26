e084320a8e6474ae2d91f9fe96b6b2480060e912
Manuel Jiménez <man.jime@gmail.com>
2020-03-26 01:03:54 -0500

    script para deploy

---
+++ backend/scripts/deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+aws cloudformation estimate-template-cost --template-body file://template.yaml
+
+aws cloudformation package --template-file template.yaml --s3-bucket transadmin.co --s3-prefix .aws/build/${1:-test} --output-template-file .aws/template.yaml
+
+aws cloudformation deploy --template-file .aws/template.yaml --stack-name transadmin-${1:-test} --capabilities CAPABILITY_NAMED_IAM --no-fail-on-empty-changeset \
+    --parameter-overrides ProjectName=transadmin Environment=${1:-test}
+
+aws cloudformation describe-stacks --stack-name transadmin-${1:-test} --query 'Stacks[0].{StackStatus:StackStatus,StackStatusReason:StackStatusReason,Outputs:Outputs}'
