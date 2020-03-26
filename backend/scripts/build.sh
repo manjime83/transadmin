3f827440f82daac12859cd16405e0f509418fff5
Manuel Jiménez <man.jime@gmail.com>
2020-03-25 20:15:35 -0500

    partail build

--- backend/scripts/build.sh
+++ backend/scripts/build.sh
@@ -1,19 +1,34 @@
 #!/bin/bash
 
-rm -rf build
+mkdir -p build/nodejs
+mkdir -p aws/layers aws/functions
 
-mkdir -p build/nodejs && cp package.json $_
-cd build/nodejs && npm install --production && cd ..
-find . -exec touch -t 8510260120 {} +
-mkdir -p ../dist/layers && zip -qr ../dist/layers/$npm_package_name.zip .
-sha256sum ../dist/layers/$npm_package_name.zip
+find build/nodejs -mindepth 1 -delete
+find aws -mindepth 1 -delete
+
+cd build/nodejs
+cp ../../package.json .
+npm install --production --no-package-lock
+rm package.json
+cd ../..
+
+echo $PWD
+
+tsc
+find build -exec touch -t 8510260120 {} +
 
 
-# tsc
-
-# cd build
 
 # find . -exec touch -t 8510260120 {} +
+# zip -qr ../dist/layers/$npm_package_name.zip .
+# cd ..
+
+
+
+
+
+
+
 
 # mkdir -p ../dist
 # for lambda in $(find . -maxdepth 1 -name "*.js" -type f); do
