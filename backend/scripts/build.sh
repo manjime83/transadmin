c8a91169d2a5bf0dc25dd7d1d0057478e7b412d8
Manuel Jiménez <man.jime@gmail.com>
2020-03-25 19:16:08 -0500

    sam removal

--- backend/scripts/build.sh
+++ backend/scripts/build.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
 rm -rf build
-tsc
-cp functions/package.json build/package.json
-npm install --production --nodedir=build
-find build -exec touch -t 8510260120 {} +
+
+mkdir -p build/nodejs && cp package.json $_
+cd build/nodejs && npm install --production && cd ..
+find . -exec touch -t 8510260120 {} +
+mkdir -p ../dist/layers && zip -qr ../dist/layers/$npm_package_name.zip .
+sha256sum ../dist/layers/$npm_package_name.zip
+
+
+# tsc
+
+# cd build
+
+# find . -exec touch -t 8510260120 {} +
+
+# mkdir -p ../dist
+# for lambda in $(find . -maxdepth 1 -name "*.js" -type f); do
+#     echo "> lambda ${lambda}..."
+#     zip -qr ../dist/$(basename $lambda).zip $lambda lib node_modules
+# done
