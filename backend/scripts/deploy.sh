fc25d1105f2a9e1c4dbadff270947a6631eb1a81
Manuel Jiménez <man.jime@gmail.com>
2020-03-28 09:14:25 -0500

    swagger

--- backend/scripts/deploy.sh
+++ backend/scripts/deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-aws s3 sync . s3://transadmin.co/.aws/${1:-test} --exclude "*" --include swagger.yaml
+curl https://api.swaggerhub.com/apis/transadmin.co/transadmin-${1:-test}/1.0.0/swagger.yaml --output .aws/swagger.yaml
+aws s3 sync .aws s3://transadmin.co/.aws/${1:-test} --exclude "*" --include swagger.yaml
 
 aws cloudformation package --template-file template.yaml --s3-bucket transadmin.co --s3-prefix .aws/${1:-test} --output-template-file .aws/template.yaml
 
