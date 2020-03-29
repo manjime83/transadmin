edafb23543770e48248243735d7adcb0c892d3a4
Manuel Jiménez <man.jime@gmail.com>
2020-03-28 23:19:42 -0500

    adding axios

--- backend/functions/aws-describe-user-pool-domain.ts
+++ backend/functions/aws-describe-user-pool-domain.ts
@@ -1,13 +1,21 @@
 import * as AWS from "aws-sdk";
-import { CloudFormationCustomResourceHandler, CloudFormationCustomResourceEvent, Context } from "aws-lambda";
-import * as response from "cfn-response";
+import {
+  CloudFormationCustomResourceHandler,
+  CloudFormationCustomResourceEvent,
+  Context,
+  CloudFormationCustomResourceSuccessResponse,
+  CloudFormationCustomResourceFailedResponse,
+} from "aws-lambda";
+import * as axios from "axios";
 
 const cognito = new AWS.CognitoIdentityServiceProvider();
 
 export const handler: CloudFormationCustomResourceHandler = async (
   event: CloudFormationCustomResourceEvent,
-  context: Context
+  _context: Context
 ): Promise<void> => {
+  console.log(event);
+
   switch (event.RequestType) {
     case "Create":
     case "Update":
@@ -16,10 +24,32 @@ export const handler: CloudFormationCustomResourceHandler = async (
         const { DomainDescription } = await cognito
           .describeUserPoolDomain({ Domain: event.ResourceProperties.Domain })
           .promise();
-        await response.send(event, context, response.SUCCESS, DomainDescription);
+
+        console.log(DomainDescription);
+
+        const response = await axios.default.put(event.ResponseURL, {
+          Status: "SUCCESS",
+          PhysicalResourceId: event.ResourceProperties.Domain,
+          StackId: event.StackId,
+          RequestId: event.RequestId,
+          LogicalResourceId: event.LogicalResourceId,
+          Data: DomainDescription,
+        } as CloudFormationCustomResourceSuccessResponse);
+
+        console.log(response.status, response.statusText, response.data);
       } catch (error) {
-        console.error("Response error:\n", error);
-        await response.send(event, context, response.FAILED);
+        console.error(error);
+
+        const response = await axios.default.put(event.ResponseURL, {
+          Status: "FAILED",
+          Reason: (error as Error).message,
+          PhysicalResourceId: event.ResourceProperties.Domain,
+          StackId: event.StackId,
+          RequestId: event.RequestId,
+          LogicalResourceId: event.LogicalResourceId,
+        } as CloudFormationCustomResourceFailedResponse);
+
+        console.log(response.status, response.statusText, response.data);
       }
       break;
   }
--- backend/scripts/build.sh
+++ backend/scripts/build.sh
@@ -26,3 +26,4 @@ rm index.js
 
 cd ..
 curl https://api.swaggerhub.com/apis/transadmin.co/transadmin-api/1.0.0/swagger.yaml --output swagger.yaml --silent
+touch -t 8510260120 swagger.yaml
