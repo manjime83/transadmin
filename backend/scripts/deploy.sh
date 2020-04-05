ffe2126b4d114cae9015623e4c80207d13ac8c8c
Manuel Jiménez <man.jime@gmail.com>
2020-04-05 09:14:09 -0500

    env files

--- backend/scripts/deploy.sh
+++ backend/scripts/deploy.sh
@@ -7,6 +7,6 @@ PROJECT_NAME='transadmin'
 aws cloudformation package --template-file template.yaml --s3-bucket $PROJECT_NAME-devops --s3-prefix $ENV --output-template-file .aws/template.yaml
 
 aws cloudformation deploy --template-file .aws/template.yaml --stack-name $PROJECT_NAME-$ENV --capabilities CAPABILITY_NAMED_IAM --no-fail-on-empty-changeset \
-    --parameter-overrides ProjectName=$PROJECT_NAME Environment=$ENV $(cat template-$ENV.env | tr -s [:space:] ' ')
+    --parameter-overrides ProjectName=$PROJECT_NAME Environment=$ENV $(cat .env.$ENV | tr -s [:space:] ' ')
 
 aws cloudformation describe-stacks --stack-name $PROJECT_NAME-$ENV --query "Stacks[0].{StackStatus:StackStatus,StackStatusReason:StackStatusReason,Outputs:Outputs}"
